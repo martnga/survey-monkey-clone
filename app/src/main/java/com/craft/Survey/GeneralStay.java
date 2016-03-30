e2ac8dfa5b395a4ebfff667050a1514b69ac96fa
martnga <mtnjoroge@gmail.com>
2016-03-30 19:36:55 +0300

    Fixed android -> app fab inflation Error

--- app/src/main/java/com/craft/Survey/GeneralStay.java
+++ app/src/main/java/com/craft/Survey/GeneralStay.java
@@ -1,17 +1,16 @@
 package com.craft.Survey;
 
 
-import android.net.Uri;
+
 
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.common.api.GoogleApiClient;
+
 
 public class GeneralStay extends AppCompatActivity {
    /* Snackbar snackbar = Snackbar
@@ -27,11 +26,6 @@ public class GeneralStay extends AppCompatActivity {
     String[] Questions = {"How enjoyable was your stay at Kapese Village?", "How would you rate the following aspects of your stay?"};
     String[] MealAspects = {"Reception", "Housekeeping", "Dining", "Maintenance", "Laundry", "Catering", "Grounds,Medical"};
 
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,46 +33,18 @@ public class GeneralStay extends AppCompatActivity {
         setContentView(R.layout.activity_general_stay);
 
         main_content = (CoordinatorLayout) findViewById(R.id.main_content);
-
+        mNextFab = (FloatingActionButton) findViewById(R.id.next_fab);
 
     }
     @Override
     public void onStart() {
         super.onStart();
 
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "GeneralStay Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app URL is correct.
-                Uri.parse("android-app://com.craft.Survey/http/host/path")
-        );
-        AppIndex.AppIndexApi.start(client, viewAction);
     }
 
     @Override
     public void onStop() {
         super.onStop();
 
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "GeneralStay Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app URL is correct.
-                Uri.parse("android-app://com.craft.Survey/http/host/path")
-        );
-        AppIndex.AppIndexApi.end(client, viewAction);
-        client.disconnect();
     }
 }
